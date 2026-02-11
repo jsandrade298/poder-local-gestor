bccf0cadb52e6112cc9b6778b37b77ba2577a2f4
jsandrade298 <js.andrade298@gmail.com>
2026-02-11 15:38:52 -0300

    Update ExportarMapaModal.tsx

--- src/components/forms/ExportarMapaModal.tsx
+++ src/components/forms/ExportarMapaModal.tsx
@@ -775,6 +775,9 @@ export function ExportarMapaModal({
                         modoVisualizacao={modoVisualizacao}
                         tipoFiltro={tipoFiltro}
                         colorirPorDensidade={colorirPorDensidade}
+                        preenchimento={camada.propriedades?.preenchimento !== false}
+                        estiloContorno={camada.propriedades?.estilo_contorno || 'solido'}
+                        espessuraContorno={camada.propriedades?.espessura_contorno || 1}
                       />
                     ))}
 
