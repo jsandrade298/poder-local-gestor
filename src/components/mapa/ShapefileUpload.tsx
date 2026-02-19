cd91342899974ce0209ee0e885781d14bcf6126d
jsandrade298 <js.andrade298@gmail.com>
2026-02-18 22:38:03 -0300

    Adjust padding in ShapefileUpload component
    Updated padding in the shapefile upload component.

--- src/components/mapa/ShapefileUpload.tsx
+++ src/components/mapa/ShapefileUpload.tsx
@@ -622,7 +622,7 @@ export function ShapefileUpload({ onUploadComplete }: ShapefileUploadProps) {
           </DialogDescription>
         </DialogHeader>
 
-        <div className="space-y-4 py-4 overflow-y-auto flex-1 min-h-0">
+        <div className="space-y-4 py-4 px-1 overflow-y-auto flex-1 min-h-0">
           {/* Upload de Arquivo */}
           <div className="space-y-2">
             <Label>Arquivo (.zip ou .geojson)</Label>
